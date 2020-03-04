bbbc466779457f8016a98a1e7527c12b9d6cfd2b
Gustavo Olivares <1390863+guolivar@users.noreply.github.com>
2020-03-05 10:24:34 +1300

    Apparently working now

--- multilogger.py
+++ multilogger.py
@@ -14,178 +14,109 @@ timestamp = time.strftime("%Y/%m/%d %H:%M:%S GMT",rec_time)
 prev_minute=rec_time[4]
 # Read the settings from the settings file
 settings_file = open("./settings.txt")
+# How many serial ports? -- MUST match the number of serial config lines in the settings file
 nports = eval(settings_file.readline().rstrip('\n'))
-ports = []
+# Initialise the lists of data to hold parameters for each instrument
 prefixes = []
+ports = []
+bauds = []
+xpars = []
+xbytes = []
+eols = []
+# Read each of the serial config lines from the settings file
 for i in range(nports):
-    # e.g. "instrument,/dev/ttyUSB0"
-    prefixes.append(settings_file.readline().rstrip('\n').split(',')[0])
-    ports.append(settings_file.readline().rstrip('\n').split(',')[1])
-    print(ports)
+    # e.g. "instrumentX,/dev/ttyUSB0,9600,N,8,n"
+    settings_line = settings_file.readline().rstrip('\n').split(',')
+    prefixes.append(settings_line[0])
+    ports.append(settings_line[1])
+    bauds.append(settings_line[2])
+    xpars.append(settings_line[3])
+    xbytes.append(eval(settings_line[4]))
+    if settings_line[5] == 'r':
+            eols.append(b'\r')
+    elif settings_line[5] == 'nr':
+        eols.append(b'\n\r')
+    else:
+        eols.append(b'\n')
+print(ports)
 # path for data files
-# e.g. "/home/logger/datacpc3775/"
+# e.g. "/home/logger/data/"
 datapath = settings_file.readline().rstrip('\n')
 print(datapath)
-flags = settings_file.readline().rstrip().split(',')
-print(timestamp + flags[0])
-
+# Format for filenames
+fnamefmt = "%Y%m%d.txt"
+# LOG some info
 current_LOG_name = datapath + time.strftime("%Y%m%d.LOG", rec_time)
 current_file = open(current_LOG_name, "a")
 current_file.write(timestamp + " Logging starts\n")
-current_file.write(timestamp + " " + prefixes + "\n")
-current_file.write(timestamp + " " + ports + "\n")
-current_file.write(timestamp + " " + datapath + "\n")
-current_file.write(timestamp + " " + flags[0] + "\n")
-current_file.flush()
-current_file.close()
+current_file.write(",".join(prefixes))
+current_file.write("\n")
+current_file.write(",".join(ports))
+current_file.write("\n")
+current_file.write(",".join(bauds))
+current_file.write("\n")
+current_file.write(",".join(xpars))
+current_file.write("\n")
+current_file.write(",".join(str(xbytes)))
+current_file.write("\n")
+current_file.write(",".join(str(eols)))
+current_file.write("\n")
+current_file.write(datapath)
+current_file.write("\n")
+
+# Setup the Serial ports for communication
+ser = []
+for i in range(nports):
+    current_file.write("Opening port " + ports[i] + "\n")
+    ser.append(serial.Serial())
+    ser[i].port = ports[i]
+    ser[i].baudrate = bauds[i]
+    ser[i].parity = xpars[i]
+    ser[i].bytesize = xbytes[i]
+    ser[i].open()
+    ser[i].flushInput()
+    ser[i].flushOutput()
+    current_file.write("Port " + ports[i] + " flushed\n")
 
 # Close the settings file
 settings_file.close()
+# Close the LOG file for now
+current_file.flush()
+current_file.close()
 
 # Start the logging
 while True:
-	try:
-		print('Setting up Serial Ports')
-        for port in ports:
-            # Open the serial port and clean the I/O buffer
-            ser = serial.Serial(port,9600,parity = serial.PARITY_EVEN,bytesize = serial.SEVENBITS, rtscts=1, stopbits=2)
-            ser.flushInput()
-            ser.flushOutput()
+    for i in range(nports):
+        try:
+            # Hacks to work with custom end of line
+            leneol = len(eols[i])
+            bline = bytearray()
+            # Get a line of data from the port
+            while True:
+                c = ser[i].read(1)
+                bline += c
+                if bline[-leneol:] == eols[i]:
+                    break
+            ## Parse the data line
+            line = bline.decode("utf-8").rstrip()
             # Set the time for the record
             rec_time=time.gmtime()
-            # Set the time for the next record (add seconds to current time)
-            rec_time_s = int(time.time()) + 60
             timestamp = time.strftime("%Y/%m/%d %H:%M:%S GMT",rec_time)
+            # Build the line to save to file
+            file_line = timestamp + "\t" + line
             # Save it to the appropriate file
-            current_file_name = datapath+time.strftime("%Y%m%d.txt",rec_time)
+            current_file_name = datapath + prefixes[i] + time.strftime("_%Y%m%d.txt",rec_time)
             current_file = open(current_file_name,"a")
             current_file.write(file_line+"\n")
             current_file.flush()
             current_file.close()
             file_line = ""
-            ser.close()
-	except:
-        current_LOG_name = datapath + time.strftime("%Y%m%d.LOG", rec_time)
-        current_file = open(current_LOG_name, "a")
-        current_file.write(timestamp + " Something unexpected happened and data wasn't logged\n")
-        current_file.flush()
-        current_file.close()
-	# Wait until the next sample time
-	while int(time.time())<=(rec_time_s):
-		#wait a few miliseconds
-		time.sleep(0.05)
+            bline = bytearray()
+        except:
+            current_LOG_name = datapath + time.strftime("%Y%m%d.LOG", rec_time)
+            current_file = open(current_LOG_name, "a")
+            current_file.write(timestamp + " Unexpected error with port " + ports[i] + "\n")
+            current_file.write(timestamp + " No data recorded\n")
+            current_file.flush()
+            current_file.close()
 print('I\'m done now')
-
-
-
-###########################################
-####### This is from the SELL #############
-####### It needs to be integrated #########
-###########################################
-# #!/usr/bin/env python
-
-# Load the libraries
-import serial # Serial communications
-import time # Timing utilities
-import subprocess # Shell utilities ... compressing data files
-# logging
-import logging
-from logging.config import fileConfig
-
-class SerialListenerLogger(object):
-	'''
-	Serial Listener Logger.
-	'''
-
-	def __init__(self):
-		logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.INFO)
-		logger = logging.getLogger(__name__)
-
-	# Localtime or UTC date wrapper
-	def get_time(self, selector):
-		if (selector=='local'):
-			return time.localtime()
-		else:
-			return time.gmtime()
-
-	def log(self, settings_file):
-		# Read the settings from the settings file
-		settings_file = open(settings_file)
-		# e.g. "/dev/ttyUSB0"
-		settings_line = settings_file.readline().rstrip('\n').split(',')
-		port = settings_line[0]
-		baud = eval(settings_line[1])
-		par  = settings_line[2]
-		byte = eval(settings_line[3])
-		ceol = settings_line[4]
-		if ceol == 'r':
-			eol = b'\r'
-		elif ceol == 'nr':
-			eol = b'\n\r'
-		else:
-			eol = b'\n'
-		logger.info(port)
-		# path for data files
-		# e.g. "/home/logger/datacpc3010/"
-		datapath = settings_file.readline().rstrip('\n')
-		logger.info(datapath)
-		filetimeformat = settings_file.readline().rstrip('\n').split(',')
-		logger.info(filetimeformat)
-		# Short or long file name format
-		if (filetimeformat[0]=='short'):
-			fnamefmt = "%Y%m%d.tsv"
-		else:
-			fnamefmt = "%Y-%m-%d.tsv"
-		# Read the compressing flag
-		flags = settings_file.readline().rstrip().split(',')
-		# Close the settings file
-		settings_file.close()
-		# Set the time constants
-		rec_time=self.get_time(filetimeformat[1])
-		timestamp = time.strftime("%Y-%m-%d\t%H:%M:%S",rec_time)
-		# Previous file name
-		prev_file_name = datapath+time.strftime(fnamefmt,rec_time)
-		# Hacks to work with custom end of line
-		leneol = len(eol)
-		logger.info(leneol)
-		bline = bytearray()
-		# Open the serial port and clean the I/O buffer
-		ser = serial.Serial()
-		ser.port = port
-		ser.baudrate = baud
-		ser.parity = par
-		ser.bytesize = byte
-		ser.open()
-		ser.flushInput()
-		ser.flushOutput()
-		while True:
-			# Get a line of data from the instrument
-			while True:
-				c = ser.read(1)
-				bline += c
-				if bline[-leneol:] == eol:
-					break
-			## Parse the data line
-			line = bline.decode("utf-8").rstrip()
-			#line = ser.readline()
-			# Set the time for the record
-			rec_time_s = int(time.time())
-			rec_time=rec_time=get_time(filetimeformat[1])
-			timestamp = time.strftime("%Y-%m-%d\t%H:%M:%S",rec_time)
-			file_line = timestamp+'\t'+line
-			logger.info(file_line)
-			# Save it to the appropriate file
-			current_file_name = datapath+time.strftime(fnamefmt,rec_time)
-			current_file = open(current_file_name,"a")
-			current_file.write(file_line+"\n")
-			current_file.flush()
-			current_file.close()
-			line = ""
-			bline = bytearray()
-			# Compress data if required
-			# Is it the last minute of the day?
-			if flags[0] == 1:
-				if current_file_name != prev_file_name:
-					subprocess.call(["gzip",prev_file_name])
-					prev_file_name = current_file_name	
-		logger.info('I\'m done')
